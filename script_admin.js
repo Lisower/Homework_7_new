23fc16d9ef4a47727c452943db7694291bf610dd
Lisower <144545336+Lisower@users.noreply.github.com>
2024-05-13 15:17:34 +0300

    Update script_admin.js

--- script_admin.js
+++ script_admin.js
@@ -5,22 +5,20 @@ const Stats = document.getElementById('Stats');
 const Form = document.getElementById('Form');
 
 Button_Stats.addEventListener('click', () => {
-    
-    /*
     fetch('./admin.php?action=fetch_stats')
-        .then(response => response.json())
+        // .then(response => response.json())
         .then(data => {
             const table = document.createElement('table');
             let html = '<tr><th>Язык программирования</th><th>Количество использований</th></tr>';
-            data.forEach(row => {
+            /*data.forEach(row => {
                 html += `<tr><td>${row.name}</td><td>${row.count}</td></tr>`;
-            });
+            }); /*
             table.innerHTML = html;
             Popup.innerHTML = '';
             Popup.appendChild(table);
             Popup.style.display = 'block';
         })
-        .catch(error => console.error('Ошибка:', error)); */
+        .catch(error => console.error('Ошибка:', error));
 });
 
 Button_Change.addEventListener('click', () => {
