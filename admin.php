0029d49718838bc579a231fd18d4431c9de51f10
Lisower <144545336+Lisower@users.noreply.github.com>
2024-05-13 15:05:49 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -17,6 +17,7 @@ if (empty($_SERVER['PHP_AUTH_USER']) ||
 
 if (isset($_GET['action']) && $_GET['action'] == 'fetch_stats') {
     header('Content-Type: application/json');
+    print(json_encode(getLanguageStats());
     echo json_encode(getLanguageStats());
     exit();
 }
