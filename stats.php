52926e21242bb4262a198836821bef8a65358691
Lisower <144545336+Lisower@users.noreply.github.com>
2024-05-21 22:52:17 +0300

    Update stats.php

--- stats.php
+++ stats.php
@@ -1,6 +1,18 @@
 <link rel="stylesheet" href="style.css">
 <?php
 require 'db.php';
+$adminData = db_select('Admin', 'login, password');
+
+if (empty($_SERVER['PHP_AUTH_USER']) ||
+    empty($_SERVER['PHP_AUTH_PW']) ||
+    $_SERVER['PHP_AUTH_USER'] != $adminData[0]['login'] ||
+    substr(md5($_SERVER['PHP_AUTH_PW']), 0, 20) != $adminData[0]['password']) {
+  header('HTTP/1.1 401 Unauthorized');
+  header('WWW-Authenticate: Basic realm="My site"');
+  print('<h1>401 Требуется авторизация</h1>');
+  exit();
+}
+
 $statsData = getLanguageStats(); ?>
 
 <!DOCTYPE html>
