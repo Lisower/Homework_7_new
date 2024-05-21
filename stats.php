5b3636c509a631669a7bd245ad84825bd8b94197
Lisower <144545336+Lisower@users.noreply.github.com>
2024-05-21 22:46:22 +0300

    Update stats.php

--- stats.php
+++ stats.php
@@ -8,24 +8,10 @@ $statsData = getLanguageStats(); ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Statistics</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-        th, td {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
+    <title>Статистика ответов</title>
 </head>
 <body>
-    <h1>Statistics</h1>
+    <h1>Статистика ответов</h1>
     <table>
         <tr>
             <th>Язык программирования</th>
@@ -33,8 +19,8 @@ $statsData = getLanguageStats(); ?>
         </tr>
         <?php foreach ($statsData as $row): ?>
             <tr>
-                <td><?php echo $row->language; ?></td>
-                <td><?php echo $row->usage_count; ?></td>
+                <td><?php echo $row->name; ?></td>
+                <td><?php echo $row->count; ?></td>
             </tr>
         <?php endforeach; ?>
     </table>
