dd4b924ff70a8630336b2e61e1e3f6be94158ec8
Lisower <144545336+Lisower@users.noreply.github.com>
2024-05-22 19:53:38 +0300

    Update login.php

--- login.php
+++ login.php
@@ -2,9 +2,14 @@
 <?php
 require 'db.php';
 header('Content-Type: text/html; charset=UTF-8');
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
-// В суперглобальном массиве $_SESSION хранятся переменные сессии.
-// Будем сохранять туда логин после успешной авторизации.
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
 $session_started = false;
 if ($_COOKIE[session_name()] && session_start()) {
   $session_started = true;
@@ -18,15 +23,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 ?>
 
 <form action="" method="post">
-  <input name="login" />
-  <input name="pass" />
-  <input type="submit" value="Войти" />
+    <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+    <input name="login" />
+    <input name="pass" />
+    <input type="submit" value="Войти" />
 </form>
 
 <?php
 }
 
 else {
+  if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        die('CSRF token validation failed');
+  }
   $result = db_select('Applications', 'id', 'login = ? and pass = ?', [$_POST['login'], substr(md5($_POST['pass']), 0, 7)]);
   if (empty($result)) {
       print('Пользователя с такими логином и паролем нет в базе данных!');
